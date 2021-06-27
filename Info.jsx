a2dbc118a13933c07880ea0879fa028b245a1896
Abhilash227 <86566295+Abhilash227@users.noreply.github.com>
2021-06-27 21:55:30 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info(){
+  return(
+    <div class="note">
+        <h1 class="note">Javascript and React.js</h1>
+        <p class="note">A Basic web dev React Js Bootcamp</p>
+      </div>
+    );
+  }
+
+export default Info;
